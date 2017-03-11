feded7c6005f82c5b5306420f057e283513b103a
Devwork <285088180@qq.com>
2017-03-11 17:33:41 +0800

    修复Bug，cancelAnimationFrame部分手机上无效

--- src/js/zepto-adapter.js
+++ src/js/zepto-adapter.js
@@ -92,12 +92,23 @@
         }
     };
     $.cancelAnimationFrame = function (id) {
-        if (window.cancelAnimationFrame) return window.cancelAnimationFrame(id);
-        else if (window.webkitCancelAnimationFrame) return window.webkitCancelAnimationFrame(id);
-        else if (window.mozCancelAnimationFrame) return window.mozCancelAnimationFrame(id);
+        var vendors = ['ms', 'moz', 'webkit', 'o'];
+        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+            window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+        }
+        if (window.cancelAnimationFrame){
+            return window.cancelAnimationFrame(id);
+        }
         else {
             return window.clearTimeout(id);
         }
+        // if (window.cancelAnimationFrame) return window.cancelAnimationFrame(id);
+        // else if (window.webkitCancelAnimationFrame) return window.webkitCancelAnimationFrame(id);
+        // else if (window.mozCancelAnimationFrame) return window.mozCancelAnimationFrame(id);
+        // else {
+        //     return window.clearTimeout(id);
+        // }
     };
     /* jshint ignore:end */
 
